d0bbc6f799a56b7b1f1cce07f7c0b6cca15a51b8
Igor Melo <igorjmelo4@gmail.com>
2021-05-24 23:04:27 -0300

    navigaiton + forms + icon and fonts

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,25 +1,51 @@
 import React from "react";
-import { SafeAreaView, Text, Image, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { useNavigation } from "@react-navigation/core";
 
-import colors from "../../styles/colors";
+import { Feather } from "@expo/vector-icons";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
 import wateringImg from "../assets/watering.png";
-import { Button } from "../components/Button";
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  function handleStart() {
+    navigation.navigate("UserIdentification");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"} suas plantas {"\n"} de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"} suas plantas de {"\n"} forma fácil
+        </Text>
 
-      <Image style={styles.image} source={wateringImg} />
+        <Image style={styles.image} source={wateringImg} resizeMode="contain" />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
 
-      <Button title=">" />
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon}></Feather>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -27,21 +53,31 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: "bold",
     textAlign: "center",
     color: colors.heading,
     marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
   },
   button: {
     backgroundColor: colors.green,
@@ -52,12 +88,8 @@ const styles = StyleSheet.create({
     height: 56,
     width: 56,
   },
-  image: {
-    width: 292,
-    height: 284,
-  },
-  buttonText: {
+  buttonIcon: {
     color: colors.white,
-    fontSize: 24,
+    fontSize: 32,
   },
 });
