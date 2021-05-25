3997f4545255562370798e4d0ffaaf1f6061c982
Igor Melo <igorjmelo4@gmail.com>
2021-05-24 23:08:52 -0300

    add TouchableWithoutFeedback

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -6,7 +6,9 @@ import {
   Text,
   TextInput,
   KeyboardAvoidingView,
+  TouchableWithoutFeedback,
   Platform,
+  Keyboard,
 } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 
@@ -46,28 +48,32 @@ export function UserIdentification() {
         style={styles.content}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        <View style={styles.content}>
-          <View style={styles.form}>
-            <View style={styles.header}>
-              <Text style={styles.emoji}>{isFilled ? "😄" : "😃"}</Text>
-              <Text style={styles.title}>Como podemos {"\n"} chamar você?</Text>
-            </View>
-            <TextInput
-              style={[
-                styles.input,
-                (isFocused || isFilled) && { borderColor: colors.green },
-              ]}
-              placeholder="Digite um nome"
-              onBlur={handleInputBlur}
-              onFocus={handleInputFocus}
-              onChangeText={handleInputChange}
-            />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.header}>
+                <Text style={styles.emoji}>{isFilled ? "😄" : "😃"}</Text>
+                <Text style={styles.title}>
+                  Como podemos {"\n"} chamar você?
+                </Text>
+              </View>
+              <TextInput
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && { borderColor: colors.green },
+                ]}
+                placeholder="Digite um nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
 
-            <View style={styles.footer}>
-              <Button title="Confirmar" onPress={handleSubmit} />
+              <View style={styles.footer}>
+                <Button title="Confirmar" onPress={handleSubmit} />
+              </View>
             </View>
           </View>
-        </View>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
