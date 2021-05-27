51b6f561e5d851ad3d688ce45a4f083358112ea0
Igor Melo <igorjmelo4@gmail.com>
2021-05-26 23:20:50 -0300

    api + enviromentbutton component

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,13 +1,53 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { EnviromentButton } from "../components/EnviromentButton";
+
 import { Header } from "../components/Header";
+import api from "../services/api";
 
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface EnviromentProps {
+  key: string;
+  title: string;
+}
 
 export function PlantSelect() {
+  const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
+
+  useEffect(() => {
+    async function fetchEnviroment() {
+      const { data } = await api.get("plants_enviroments");
+      setEnviroments([
+        {
+          key: "all",
+          title: "Todos",
+        },
+        ...data,
+      ]);
+    }
+
+    fetchEnviroment();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Header />
+      <View style={styles.header}>
+        <Header />
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta</Text>
+      </View>
+
+      <View>
+        <FlatList
+          data={enviroments}
+          renderItem={({ item }) => <EnviromentButton title={item.title} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+        />
+      </View>
     </View>
   );
 }
@@ -18,5 +58,28 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: "center",
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
   },
 });
